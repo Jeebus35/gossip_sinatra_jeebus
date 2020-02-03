9e424416cc0113aee4c771c942383e83b00de4f8
Jean-Baptiste Tricheur <jean-baptiste.tricheur@hotmail.fr>
2020-02-03 12:02:44 +0100

    first commit

---
+++ controller.rb
@@ -0,0 +1,8 @@
+
+
+class ApplicationController < Sinatra::Base
+  get '/' do 
+    "<html><head><title>The Gossip Project</title></head><body><h1> Salut les keums !</h1></body></html>"
+  end 
+
+end 
